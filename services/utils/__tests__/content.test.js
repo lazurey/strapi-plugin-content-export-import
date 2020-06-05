55c4d7666c8cdcf54c64d5ce961f3e0a68967f61
Felipe Baytelman <felipe.baytelman@gmail.com>
2020-06-05 10:16:49 -0400

    Implement tests

--- services/utils/__tests__/content.test.js
+++ services/utils/__tests__/content.test.js
@@ -1,15 +1,22 @@
-import {findAll, importSingleType} from "../content";
+import { findAll, importSingleType, importItemByContentType } from '../content';
 
 describe('# Stripi api helpers', () => {
   const setUpStrapi = (method) => {
     global.strapi = {
       query: () => method,
+      models: {
+        uid: {
+          attributes: {
+            slug: { index: true, unique: true },
+          },
+        },
+      },
     };
   };
 
   it('should find all content by type', async () => {
     const find = jest.fn();
-    setUpStrapi({find});
+    setUpStrapi({ find });
     await findAll('uid');
     expect(find).toHaveBeenCalled();
   });
@@ -35,6 +42,38 @@ describe('# Stripi api helpers', () => {
     };
     setUpStrapi(methods);
     await importSingleType('uid', { value: 2 });
+    expect(update).not.toHaveBeenCalled();
+    expect(create).toHaveBeenCalled();
+  });
+
+  it('should update collection content if exist', async () => {
+    const update = jest.fn();
+    const create = jest.fn();
+    const methods = {
+      model: {
+        findOne: () => ({
+          id: 1,
+        }),
+      },
+      update,
+      create,
+    };
+    setUpStrapi(methods);
+    await importItemByContentType('uid', { slug: '123', value: 2 });
+    expect(update).toHaveBeenCalled();
+    expect(create).not.toHaveBeenCalled();
+  });
+
+  it('should create a new one for collection content if not exist', async () => {
+    const update = jest.fn();
+    const create = jest.fn();
+    const methods = {
+      model: { findOne: () => null },
+      update,
+      create,
+    };
+    setUpStrapi(methods);
+    await importItemByContentType('uid', { slug: '123', value: 2 });
     expect(update).not.toHaveBeenCalled();
     expect(create).toHaveBeenCalled();
   });
