c9ccc539ae8a64ebc58ed08f00d56c28d542549e
Felipe Baytelman <felipe.baytelman@gmail.com>
2020-06-05 09:41:06 -0400

    Allow upsert

--- services/utils/content.js
+++ services/utils/content.js
@@ -1,13 +1,36 @@
-const importItemByContentType = (id, item) => {
+const importItemByContentType = async (id, item) => {
+  const modelName = id.split('.').reverse()[0];
+  const model = strapi.models[modelName];
+  const uniqueIndexes = Object.keys(model.attributes).filter(
+    (aName) => model.attributes[aName].index && model.attributes[aName].unique
+  );
+  const search = [];
+  uniqueIndexes.forEach((i) => {
+    if (item[i]) {
+      search.push({
+        [i]: item[i],
+      });
+    }
+  });
+
+  if (search.length > 0) {
+    const existingItem = await strapi.query(id).model.findOne({ $or: search });
+    if (existingItem) {
+      return strapi.query(id).update({ id: existingItem.id }, item);
+    }
+  }
   return strapi.query(id).create(item);
 };
 
 const importSingleType = async (uid, item) => {
   const existing = await strapi.query(uid).find({});
   if (existing.length > 0) {
-    return strapi.query(uid).update({
-      id: existing[0].id,
-    }, item)
+    return strapi.query(uid).update(
+      {
+        id: existing[0].id,
+      },
+      item
+    );
   } else {
     return strapi.query(uid).create(item);
   }
@@ -19,7 +42,7 @@ const findAll = (uid) => {
 
 const deleteByIds = (uid, ids) => {
   return strapi.query(uid).delete({
-    id_in: ids
+    id_in: ids,
   });
 };
 
