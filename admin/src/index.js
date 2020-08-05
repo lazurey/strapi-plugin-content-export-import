8aaabc71b89213e491bf20de83f78dd34cabaa1a
José L. S. A <alagunasalahaddin@live.com>
2020-08-05 18:52:25 +0300

    Updating to the last strapi version.

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,14 +1,20 @@
-import pluginPkg from '../../package.json';
-import pluginId from './pluginId';
-import App from './containers/App';
-import Initializer from './containers/Initializer';
-import lifecycles from './lifecycles';
-import trads from './translations';
+import pluginPkg from "../../package.json";
+import pluginId from "./pluginId";
+import App from "./containers/App";
+import Initializer from "./containers/Initializer";
+import lifecycles from "./lifecycles";
+import trads from "./translations";
 
-export default strapi => {
-  const pluginDescription = pluginPkg.strapi.description || pluginPkg.description;
+export default (strapi) => {
+  const pluginDescription =
+    pluginPkg.strapi.description || pluginPkg.description;
+  const icon = pluginPkg.strapi.icon;
+  const name = pluginPkg.strapi.name;
 
   const plugin = {
+    icon,
+    name,
+    destination: `/plugins/${pluginId}`,
     blockerComponent: null,
     blockerComponentProps: {},
     description: pluginDescription,
@@ -19,13 +25,26 @@ export default strapi => {
     isReady: false,
     isRequired: pluginPkg.strapi.required || false,
     layout: null,
-    lifecycles,
     leftMenuLinks: [],
     leftMenuSections: [],
     mainComponent: App,
     name: pluginPkg.strapi.name,
     preventComponentRendering: false,
     trads,
+    menu: {
+      // Set a link into the PLUGINS section
+      pluginsSectionLinks: [
+        {
+          destination: `/plugins/${pluginId}/export`, // Endpoint of the link
+          icon,
+          name,
+          label: {
+            id: `${pluginId}.plugin.name`, // Refers to a i18n
+            defaultMessage: "Content Export & Import",
+          },
+        },
+      ],
+    },
   };
 
   return strapi.registerPlugin(plugin);
